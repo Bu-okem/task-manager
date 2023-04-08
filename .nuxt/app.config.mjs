54470a7a3524e6b2256f533340bf76da7dc1b9d8
Chibuokem Obiegbulem <chiobiegbulem@gmail.com>
2023-04-08 02:49:04 +0100

    Initial Commit

---
+++ .nuxt/app.config.mjs
@@ -0,0 +1,8 @@
+
+import { defuFn } from '/home/buokem/dev/projects/task-manager/node_modules/defu/dist/defu.mjs'
+
+const inlineConfig = {}
+
+
+
+export default /* #__PURE__ */ defuFn(inlineConfig)
